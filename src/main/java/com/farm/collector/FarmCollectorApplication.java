ca0d51cff9e59eab5bb6d75d1528b63bf3b534dd
Arun Gandhi <arungandhi@Aruns-MacBook-Air.local>
2024-07-20 15:27:00 +0530

    renaming package

--- src/main/java/com/farm/collector/FarmCollectorApplication.java
+++ src/main/java/com/farm/collector/FarmCollectorApplication.java
@@ -1,4 +1,4 @@
-package com.farm.collection.FarmCollector;
+package com.farm.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
