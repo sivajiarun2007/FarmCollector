46293178c5332b12133012bdc5b1e683d6c23b6e
Arun Gandhi <arungandhi@Aruns-MacBook-Air.local>
2024-07-20 15:19:24 +0530

    init project

---
+++ src/main/java/com/farm/collection/FarmCollector/FarmCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.farm.collection.FarmCollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmCollectorApplication.class, args);
+	}
+
+}
